483f85a856c90d0c5c16a33d3f6d0e7cca147e46
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-10 11:02:52 +0100

    error name values

--- js09.js
+++ js09.js
@@ -51,3 +51,13 @@ function myFunction() {
 }
 </script>
 */
+
+// Error name values
+
+// 1 -- EvalError An error has occurred in the eval() function
+// 2 -- RangeError  A number "out of range" has occurred
+// 3 -- ReferenceError  An illegal reference has occurred
+// 4 -- SyntaxError A syntax error has occurred
+// 5 -- TypeError A type error has occurred
+// 6 -- URIError  An error in encodeURI() has occurred
+
