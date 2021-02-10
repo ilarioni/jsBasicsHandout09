88c494572409f6673396db78a6c766c2e56cbf48
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-10 11:01:21 +0100

    error handling

---
+++ js09.js
@@ -0,0 +1,6 @@
+// JavaScript Errors - Throw and Try to Catch
+// 1 -- try -- statement lets you test a block of code for errors
+// 2 -- catch -- statement lets you handle the error. 
+// 3 -- throw -- statement lets you create custom errors. 
+// 4 -- finally -- statement lets you execute code.
+
