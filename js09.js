ee1bafad80f6c2511f9a61e55fa89e5b2b2259ab
ilarioni <ilia.meshvildishvili@gmail.com>
2021-02-10 11:01:59 +0100

    first example

--- js09.js
+++ js09.js
@@ -4,3 +4,24 @@
 // 3 -- throw -- statement lets you create custom errors. 
 // 4 -- finally -- statement lets you execute code.
 
+// First example script in html
+/*
+<script>
+function myFunction() {
+  var message, x;
+  message = document.getElementById("p01");
+  message.innerHTML = "";
+  x = document.getElementById("demo").value;
+  try { 
+    if(x == "")  throw "empty";
+    if(isNaN(x)) throw "not a number";
+    x = Number(x);
+    if(x < 5)  throw "too low";
+    if(x > 10)   throw "too high";
+  }
+  catch(err) {
+    message.innerHTML = "Input is " + err;
+  }
+}
+</script>
+*/
